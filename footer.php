4f55c63da8a0899c350e04458d62032b5529b934
ThomasHenriques <henriquesthomas3@gmail.com>
2026-09-08 11:18:56 +0200

    Edit Footer

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 <!-- footer.php -->
 <footer class="bg-light text-center text-lg-start mt-auto">
   <div class="text-center p-3">
-    © 2021 Copyright:
-    <a class="text-dark">Jackie Chan</a>
+    © 2077 Copyright:
+    <a class="text-dark">Saburo Arasaka</a>
   </div>
 </footer>
