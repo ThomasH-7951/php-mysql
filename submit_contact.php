9cffa056d32d394bb7c4de96ec953f688d906f7a
ThomasHenriques <henriquesthomas3@gmail.com>
2026-09-11 09:22:12 +0200

    Traitez l'envoi en PHP

--- submit_contact.php
+++ submit_contact.php
@@ -32,12 +32,32 @@
                 <h5 class="card-title">Rappel de vos informations</h5>
                 <p class="card-text"><b>Email</b> : <?php echo htmlspecialchars($_POST['email']); ?></p>
                 <p class="card-text"><b>Message</b> : <?php echo htmlspecialchars($_POST['message']); ?></p>
+                </br>
+                <?php
+                    // Testons si le fichier a bien été envoyé et s'il n'y a pas d'erreur
+                    if (isset($_FILES['screenshot']) && $_FILES['screenshot']['error'] == 0)
+                    {
+                        // Testons si le fichier n'est pas trop gros
+                        if ($_FILES['screenshot']['size'] <= 1000000)
+                        {
+                            // Testons si l'extension est autorisée
+                            $fileInfo = pathinfo($_FILES['screenshot']['name']);
+                            $extension = $fileInfo['extension'];
+                            $allowedExtensions = ['jpg', 'jpeg', 'gif', 'png'];
+                            if (in_array($extension, $allowedExtensions))
+                            {
+                                // On peut valider le fichier et le stocker définitivement
+                                move_uploaded_file($_FILES['screenshot']['tmp_name'],
+                                        'uploads/' . basename($_FILES['screenshot']['name']));
+                                echo "L'envoi d'image a bien été effectué !";
+                            }
+                        }
+                    }
+                ?>
             </div>
             </div>
 
-        
-        
-        
+       
        
     </div>
 
