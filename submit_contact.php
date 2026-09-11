d898c409141af993143320de1046d7380fc4b648
ThomasHenriques <henriquesthomas3@gmail.com>
2026-09-11 08:51:15 +0200

    Récupérez les paramètres en PHP

---
+++ submit_contact.php
@@ -0,0 +1,39 @@
+<!-- index.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site de recettes - Page d'accueil</title>
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet"
+    >
+</head>
+<body class="d-flex flex-column min-vh-100">
+    <div class="container">
+
+    <?php include_once('header.php'); ?>
+        <h1>Site de recettes</h1>
+
+        <h1>Message bien reçu !</h1>
+
+            <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Rappel de vos informations</h5>
+                <p class="card-text"><b>Email</b> : <?php echo $_GET['email']; ?></p>
+                <p class="card-text"><b>Message</b> : <?php echo $_GET['message']; ?></p>
+            </div>
+            </div>
+
+        
+        
+        
+       
+    </div>
+
+    <!-- inclusion du bas de page du site -->
+    <?php include_once('footer.php'); ?>
+</body>
+</html>
