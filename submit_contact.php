14f3945f28f758d174c9044df3aa41f39eaf6d92
ThomasHenriques <henriquesthomas3@gmail.com>
2026-09-11 08:58:01 +0200

    Contrôlez la valeur des paramètres

--- submit_contact.php
+++ submit_contact.php
@@ -17,11 +17,11 @@
     <?php include_once('header.php'); ?>
         <h1>Site de recettes</h1>
         <?php
-            if (!isset($_GET['email']) || !isset($_GET['message']))
+            if (
+            (!isset($_GET['email']) || !filter_var($_GET['email'], FILTER_VALIDATE_EMAIL)) || (!isset($_GET['message']) || empty($_GET['message']))
+            )
             {
-                echo('<h1>Il faut un email et un message pour soumettre le formulaire.</h1>');
-
-                // Arrête l'exécution de PHP
+                echo('Il faut un email et un message valides pour soumettre le formulaire.');
                 return;
             }
         ?>
