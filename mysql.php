a8edd3b23ab4e707d4ceaa74d9c3584265514dbf
ThomasHenriques <henriquesthomas3@gmail.com>
2026-09-17 10:54:59 +0200

    Effectuez une première requête SQL

--- mysql.php
+++ mysql.php
@@ -3,7 +3,7 @@ const MYSQL_HOST = 'localhost';
 const MYSQL_PORT = 3306;
 const MYSQL_NAME = 'my_recipes';
 const MYSQL_USER = 'root';
-const MYSQL_PASSWORD = 'root';
+const MYSQL_PASSWORD = '';
 try {
     $db = new PDO(
         sprintf(
@@ -13,7 +13,8 @@ try {
             MYSQL_PORT
         ),
         MYSQL_USER,
-        MYSQL_PASSWORD
+        MYSQL_PASSWORD,
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],
     );
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $exception) {
